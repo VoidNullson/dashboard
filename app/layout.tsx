ae1c93c483e4689d3b53275d4bd753164852f194
Void Nullson <66926318+VoidNullson@users.noreply.github.com>
2025-03-18 19:11:26 -0700

    add metadata template objects | root and invoice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | App Dashboard',
+    default: 'App Dashboard',
+  },
+  description: 'base app description.',
+  // metadataBase: new URL('example.sh')
+};
 
 export default function RootLayout({
   children,
