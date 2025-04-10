4cb44e0e8dd1c22ea08d2b3eb00b44e6ffde2fee
Void Nullson <66926318+VoidNullson@users.noreply.github.com>
2025-04-09 23:30:40 -0700

    uniform code conventions and spacing, misc files

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,22 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s | App Dashboard',
-    default: 'App Dashboard',
-  },
-  description: 'base app description.',
-  // metadataBase: new URL('example.sh')
+    title: {
+        template: '%s | App Dashboard',
+        default: 'App Dashboard',
+    },
+    description: 'base app description.',
+    // metadataBase: new URL('example.sh')
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+    children,
+    }: {
+    children: React.ReactNode;
+    }) {
+    return (
+        <html lang="en">
+        <body className={ `${inter.className} antialiased` }>{ children }</body>
+        </html>
+    );
 }
--- app/query/route.ts
+++ app/query/route.ts
@@ -8,15 +8,15 @@ async function listInvoices() {
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
-  `;
+    `;
 
 	return data;
 }
 
 export async function GET() {
-  try {
-  	return Response.json(await listInvoices());
-  } catch (error) {
-  	return Response.json({ error }, { status: 500 });
-  }
+    try {
+        return Response.json(await listInvoices());
+    } catch (error) {
+        return Response.json({ error }, { status: 500 });
+    }
 }
