f98cfbcd8974b4935a893a2a641920435edfab1d
Void Nullson <66926318+VoidNullson@users.noreply.github.com>
2025-04-10 08:21:30 -0700

    final unification of coding convention

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,17 @@
-import { Metadata } from 'next';
-import { fetchFilteredCustomers } from '@/app/lib/data';
-import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
-    title: 'Customers',
+	title: "Customers",
 };
 
-export default async function Page(props: { searchParams?: Promise<{ query?: string; page?: string; }> }) {
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || '';
-    const customers = await fetchFilteredCustomers( query );
+export default async function Page(props: {
+	searchParams?: Promise<{ query?: string; page?: string }>;
+}) {
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || "";
+	const customers = await fetchFilteredCustomers(query);
 
-    return <CustomersTable customers={ customers }/>;
+	return <CustomersTable customers={customers} />;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: {
-        template: '%s | App Dashboard',
-        default: 'App Dashboard',
-    },
-    description: 'base app description.',
-    // metadataBase: new URL('example.sh')
+	title: {
+		template: "%s | App Dashboard",
+		default: "App Dashboard",
+	},
+	description: "base app description.",
+	// metadataBase: new URL('example.sh')
 };
 
 export default function RootLayout({
-    children,
-    }: {
-    children: React.ReactNode;
-    }) {
-    return (
-        <html lang="en">
-        <body className={ `${inter.className} antialiased` }>{ children }</body>
-        </html>
-    );
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
